b56fa2b1f324eb8657e0756b2b0aa960943134a1
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-03-19 11:41:37 -0400

    Add files via upload
    added icons to use for navigation

--- resumaker_mobile/lib/app_assets.dart
+++ resumaker_mobile/lib/app_assets.dart
@@ -3,4 +3,7 @@ class Assets {
   static const String rightArrow = 'lib/assets/icons/rightArrow.svg';
   static const String placeholder = 'lib/assets/placeholders/100x100.png';
   static const String logoIcon = 'lib/assets/icons/ResumakerLogo.png';
+  static const String profileIcon = 'lib/assets/icons/profile.svg';
+  static const String scanIcon = 'lib/assets/icons/scan.svg';
+  static const String builderIcon = 'lib/assets/icons/builder.svg';
 }
