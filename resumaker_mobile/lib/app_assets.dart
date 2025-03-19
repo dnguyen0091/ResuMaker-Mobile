afde8e817a70dd0c14a16dc3be7067ce088c1c7a
David Nguyen <107872059+dnguyen0091@users.noreply.github.com>
2025-03-19 10:48:42 -0400

    Add files via upload
    new assets added as variables

--- resumaker_mobile/lib/app_assets.dart
+++ resumaker_mobile/lib/app_assets.dart
@@ -1,5 +1,6 @@
 class Assets {
-  static const String logo = 'lib/assets/icons/logo.png';
+  static const String logo = 'lib/assets/icons/Resumaker.png';
   static const String rightArrow = 'lib/assets/icons/rightArrow.svg';
   static const String placeholder = 'lib/assets/placeholders/100x100.png';
+  static const String logoIcon = 'lib/assets/icons/ResumakerLogo.png';
 }
